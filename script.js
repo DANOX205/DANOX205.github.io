3dd83850f1986519c9157e82a535ef7cd98dffcd
Anthony97480 <tony.ferrere.h@gmail.com>
2024-11-22 19:45:06 +0100

    diviser pour mieux régné
    division de la fonction de déplacement en deux fonction, une pour le déplacement, une pour codé l'attaque du personnage, et une fonction principal regroupant les deux autres

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 console.log('start')
 
-
-function deplacement_perso(event){
-	const command = event.keyCode;
-	const perso_p = document.getElementById("perso");
-	let col_val = perso_p.style.gridColumn;
-	let row_val = perso_p.style.gridRow;
+function deplacerP(command, perso){
+	let col_val = perso.style.gridColumn;
+	let row_val = perso.style.gridRow;
 	
 //vérification des valeur des coordonnée et attribution si celle-ci n'existe pas
 	if (col_val === null || row_val === null){
@@ -39,10 +36,26 @@ function deplacement_perso(event){
 	}
 	
 	//attribution des valeur après déplacement
-	perso_p.style.gridRow = row_val;
-	perso_p.style.gridColumn = col_val;
+	perso.style.gridRow = row_val;
+	perso.style.gridColumn = col_val;
+	return [row_val * 1, col_val * 1];
 }
 
+function attaqueP(command, perso){
+	alert("raiponse ATTAQUE");//à compléter
+}
+
+function PersoPrinc(event){
+	const command = event.keyCode;
+	const perso_p = document.getElementById("perso");
+	if (command === 122 || command === 113 || command === 115 || command === 100){
+		let tab_coord = deplacerP(command, perso_p);
+		perso_p.style.gridRow = tab_coord[0];
+		perso_p.style.gridColumn = tab_coord[1];
+	} else if (command === 101){ //e press
+		attaqueP(command, perso_p);
+	}
+}
 
 /* Gestion du Chrono */
 let secondes = 0;
