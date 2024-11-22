b80ff19c531b07fee22972168c24ec5ba2413e10
Anthony97480 <tony.ferrere.h@gmail.com>
2024-11-22 18:18:08 +0100

    fonction de déplacement du personnage
    réalisation des fonction de déplacement du personnage (il faut cliquer sur la page  puis se servir des touche du clavier (zqsd) pour le déplacement

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
 console.log('start')
+let x = 0;
+let y = 0;
+let tab_coord = [x, y];
 
 
 function deplacement_perso(event){
 	const command = event.keyCode;
 	const perso_p = document.getElementById("perso");
-	let col_val = perso_p.getAttribute("grid-columns");
-	let row_val = perso_p.getAttribute("grid-rows");
-	console.log("col " + col_val);
-	console.log("row " + row_val);
+	let col_val = perso_p.style.gridColumn;
+	let row_val = perso_p.style.gridRow;
+	
+	if (col_val === null || row_val === null){
+		col_val = 0;
+		row_val = 0;
+	}
+	
 	if (command === 122){ //z press
-		if (row_val){
+		if (row_val > 0){
 			row_val = (row_val * 1) - 1;
 		}
-		perso_p.setAttribute("grid-rows", row_val);
 	} else if (command === 113){ //q press
-		if (col_val){
+		if (col_val > 0){
 			col_val = (col_val * 1) - 1;
 		}
-		perso_p.setAttribute("grid-columns", col_val);
 	} else if (command === 115){ //s press
 		row_val = (row_val * 1) + 1;
-		perso_p.setAttribute("grid-rows", row_val);
 	} else if (command === 100){ //d press
 		col_val = (col_val * 1) + 1;
-		perso_p.setAttribute("grid-columns", col_val);
 	}
+	perso_p.style.gridRow = row_val;
+	perso_p.style.gridColumn = col_val;
+	console.log("col " + col_val);
+	console.log("row " + row_val);
 }
 
 
