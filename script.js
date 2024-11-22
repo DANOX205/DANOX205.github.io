ee853a0be5e8feb08b626b2d4d8ba2bb3c0d64c4
Anthony97480 <tony.ferrere.h@gmail.com>
2024-11-22 16:55:06 +0100

    création des déplacement

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 console.log('start')
 
+function deplacement_perso(event){
+	const command = event.keyCode;
+	const perso_p = document.getElementById("perso");
+	let col_val = perso_p.getAttribute("grid-columns");
+	let row_val = perso_p.getAttribute("grid-rows");
+	console.log("col " + col_val);
+	console.log("row " + row_val);
+	if (command === 122){ //z press
+		if (row_val){
+			row_val = (row_val * 1) - 1;
+		}
+		perso_p.setAttribute("grid-rows", row_val);
+	} else if (command === 113){ //q press
+		if (col_val){
+			col_val = (col_val * 1) - 1;
+		}
+		perso_p.setAttribute("grid-columns", col_val);
+	} else if (command === 115){ //s press
+		row_val = (row_val * 1) + 1;
+		perso_p.setAttribute("grid-rows", row_val);
+	} else if (command === 100){ //d press
+		col_val = (col_val * 1) + 1;
+		perso_p.setAttribute("grid-columns", col_val);
+	}
+}
