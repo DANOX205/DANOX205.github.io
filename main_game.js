79c13af16bc89b4e98d9e8605471da32da045941
Anthony97480 <tony.ferrere.h@gmail.com>
2024-11-27 16:27:32 +0100

    fix du bug game_box

--- main_game.js
+++ main_game.js
@@ -1,27 +1,14 @@
 //page javascript pour la page du jeux
 console.log('start game')
+const player = document.querySelector("#player");
+const gameWorld = document.querySelector("#gameWorld");
 const gameBox= document.querySelector(".game_box");
 const Clock = document.querySelector("#clock");
+const attq_p = document.querySelector("#att_player");
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
-
-
-//key code des lettre de l'alphabet
-const z = 122;
-const q = 113;
-const s = 115;
-const d = 100;
-const e = 101;
-const space = 32; //barre d'espace
-
-
-//récupération du pseudo de la page principal
-const Jpseudo = localStorage.getItem('pseudo_perso');
-const labelP = document.getElementById("pseudo");
-localStorage.removeItem('pseudo_perso'); //libération du stockage
-labelP.textContent = Jpseudo; //écritude du pseudo au dessus du personnage
 
 
 //fonction de fin de jeux, sauvegarde du temps et retour à la page principal
@@ -31,103 +18,15 @@ function GameOver(){
 	window.location.href = 'Projet_Web.html';
 }
 
-//fonction de déplacement et d'attaque du personnage
-function deplacement(command, perso, attq){
-	let col_val = perso.style.gridColumn;
-	let row_val = perso.style.gridRow;
-	
-//vérification des valeur des coordonnée et attribution si celle-ci n'existe pas
-	if (col_val === null || row_val === null){
-		col_val = 0;
-		row_val = 0;
-	}
-	
-	
-	//commende de déplacement en fonction du code clavier des touches
-	//on ajoute/retranche 1 à la ligne/colonne de l'objet pour le déplacer
-	if (command === z){
-		if (row_val > 0){ //vérification que les coordonnée ne soit pas négative
-			row_val = (row_val * 1) - 1;
-		}
-	}
-	if (command === q){
-		if (col_val > 0){ //vérification que les coordonnée ne soit pas négative
-			col_val = (col_val * 1) - 1;
-		}
-	}
-	if (command === s){
-		if (row_val < 22){ //vérification que le personnage ne quitte pas la zone de jeux
-			row_val = (row_val * 1) + 1;
-		}
-	}
-	if (command === d){
-		if (col_val < 55){ //vérification que le personnage ne quitte pas la zone de jeux
-			col_val = (col_val * 1) + 1;
-		}
-	}
-	
-	//attribution des valeur après déplacement
-	perso.style.gridRow = row_val;
-	perso.style.gridColumn = col_val;
-	
-	//déplacement de l'attaque du personnage
-	if (command === z){
-		attq.style.gridRow = (perso.style.gridRow * 1) - 1;
-		attq.style.gridColumn = (perso.style.gridColumn * 1);
-	}
-	if (command === q){
-		attq.style.gridRow = (perso.style.gridRow * 1);
-		attq.style.gridColumn = (perso.style.gridColumn * 1) - 1;
-	}
-	if (command === s){
-		attq.style.gridRow = (perso.style.gridRow * 1) + 2;
-		attq.style.gridColumn = (perso.style.gridColumn * 1);
-	}
-	if (command === d){
-		attq.style.gridRow = (perso.style.gridRow * 1);
-		attq.style.gridColumn = (perso.style.gridColumn * 1) + 2;
-	}
-	
-	return [row_val * 1, col_val * 1];
-}
 
-function attaqueP(command, attq){
+function attaqueP(attq){
 	attq.style.display = "block";
 	sleep(2000).then( () => {
 		attq.style.display = "none";
 	});
 }
 
-function Player(event){
-	const command = event.keyCode;
-	console.log(command);
-	const perso = document.getElementById("player");
-	const attq_p = document.getElementById("att_player");
-	if (command === z || command === q || command === s || command === d){
-		let tab_coord = deplacement(command, perso, attq_p);
-		//initialisation de la position du personnage
-		perso.style.gridRow = tab_coord[0];
-		perso.style.gridColumn = tab_coord[1];
-		
-		//initialisation de la position du pseudo
-		labelP.style.gridRow = (tab_coord[0] * 1) - 2
-		labelP.style.gridColumn = (tab_coord[1] * 1) - 1;
-	} else if (command === e){
-		attaqueP(command, attq_p);
-	} else if (command === space){ //ou dituation d'echec
-		alert("Game Over");
-		GameOver();
-	}
-}
-
 //--------------------------------------------------
-console.log('start game')
-const player = document.querySelector("#player");
-const gameWorld = document.querySelector("#gameWorld");
-const gameBox= document.querySelector(".game_box");
-const Clock = document.querySelector("#clock");
-
-console.log("LE JS est bien ouvert!");
 
 // Player's position
 const playerState = {
@@ -147,7 +46,16 @@ document.addEventListener("keyup", (e) => {
   keys[e.key] = false; // Mark key as released
 });
 
+
 //Move the player
+
+
+//récupération du pseudo de la page principal
+const Jpseudo = localStorage.getItem('pseudo_perso');
+const labelP = document.getElementById("pseudo");
+//localStorage.removeItem('pseudo_perso'); //libération du stockage
+labelP.textContent = Jpseudo; //écritude du pseudo au dessus du personnage
+
 
 function updatePlayer() {
   // Move up
@@ -166,16 +74,33 @@ function updatePlayer() {
   if (keys["d"]) {
     playerState.x = Math.min(gameWorld.offsetWidth - player.offsetWidth, playerState.x + playerState.speed);
   }
+  //Game over
+  if (keys["space"]) {
+	alert("game over");
+	GameOver();
+  }
 
   // Apply new position
   player.style.top = `${playerState.y}px`;
   player.style.left = `${playerState.x}px`;
+  //player's attack
+  if (keys["e"]) {
+	attq_p.style.top = player.style.top;
+	attq_p.style.top = player.style.left;
+	attaqueP(attq_p);
+  }
+
+  //new position for the label
+  labelP.style.top = `${playerState.y}px`;
+  labelP.style.left = `${playerState.x}px`;
 }
 
 function gameLoop() {
   updatePlayer(); // Update player's position
   requestAnimationFrame(gameLoop); // Repeat the loop
 }
+
+gameLoop();
 //--------------------------------------------------
 
 
