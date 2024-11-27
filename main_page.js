b3d94c67c3f93398b45a848f6e4ef516b28819b7
Mathis <87679380+Mathiiis@users.noreply.github.com>
2024-11-27 17:01:37 +0100

    2 joueur

--- main_page.js
+++ main_page.js
@@ -10,9 +10,11 @@ function Pseudo_ok(pseudo){
 }
 
 function StartGame(){ //fonction de redirection vers la page du jeux
-	const Jpseudo = document.getElementById("idpseudo").value;
-	if (Pseudo_ok(Jpseudo)){
-		localStorage.setItem('pseudo_perso', Jpseudo); //stockage du pseudo
+	const Jpseudo1 = document.getElementById("idpseudo1").value;
+	const Jpseudo2 = document.getElementById("idpseudo2").value;
+	if (Pseudo_ok(Jpseudo1 && Jpseudo2)){
+		localStorage.setItem('pseudo_perso1', Jpseudo1); //stockage du pseudo
+		localStorage.setItem('pseudo_perso2', Jpseudo2); //stockage du pseudo
 		window.location.href = "main_game.html" //redirection sur la page du jeux
 	} else {
 		alert("Veuillez entrer un pseudo correct pour continuer !");
