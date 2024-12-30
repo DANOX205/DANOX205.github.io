16d8ea7047c08ea73071604bdb3f658893ff0f59
DANOX205 <79769376+DANOX205@users.noreply.github.com>
2024-12-30 12:14:51 +0100

    Update main_game.js

--- main_game.js
+++ main_game.js
@@ -90,31 +90,46 @@ document.addEventListener("keyup", (e) => {
 });
 
 let command = "z";
+let facing = "left";
+let moving = false;
 function updatePlayer() {
 	// Move up
 	if (keys["z"] || keys["Z"]) {
 		playerState.y = Math.max(0, playerState.y - playerState.speed);
 		command = "z";
+		moving = true;
 	}
 	// Move down
 	if (keys["s"] || keys["S"]) {
 		playerState.y = Math.min(gameWorld.offsetHeight - player.offsetHeight, playerState.y + playerState.speed);
-		command = "s"
+		command = "s";
+		moving = true;
 	}
 	// Move left
 	if (keys["q"] || keys["Q"]) {
 		playerState.x = Math.max(0, playerState.x - playerState.speed);
-		command = "q"
+		command = "q";
+		facing = "left";
+		moving = true;
 	}
 	// Move right
 	if (keys["d"] || keys["D"]) {
 		playerState.x = Math.min(gameWorld.offsetWidth - player.offsetWidth, playerState.x + playerState.speed);
-		command = "d"
+		command = "d";
+		facing = "right";
+		moving = true;
+	}
+
+	// On ne touche aucune des touches de déplacement.
+	if (!((keys["q"] || keys["Q"] || keys["d"] || keys["D"] || keys["s"] || keys["S"] || keys["z"] || keys["Z"]) || ((keys["q"] || keys["Q"])&&(keys["d"] || keys["D"])) || ((keys["z"] || keys["Z"])&&(keys["s"] || keys["S"])))) {
+		moving = false;
 	}
 
 	// Apply new position
 	player.style.top = `${playerState.y}px`;
 	player.style.left = `${playerState.x}px`;
+	drawingPlayer.style.top = `${playerState.y-60}px`;
+	drawingPlayer.style.left = `${playerState.x-29}px`;
 	
   	//player's attack position
 	let att_T = 0;
@@ -161,6 +176,8 @@ function updatePlayer() {
 		// Apply new position
 		moving_cloud.style.top = `${moving_cloudState.y}px`;
 		moving_cloud.style.left = `${moving_cloudState.x}px`;
+		drawingMovingCloud.style.top = `${moving_cloudState.y-248}px`;
+		drawingMovingCloud.style.left = `${moving_cloudState.x-144}px`;
 	}
 	labelD.textContent = degat;
 	if (collision.length != 0){
@@ -190,6 +207,128 @@ function gameLoop() {
 }
 
 gameLoop();
+
+// On met à jour l'animation du Joueur
+function animation_Joueur(){
+
+	if (facing==="left"){
+		drawingPlayer.style.transform  = `scale(1, 1)`;
+		if (moving ===true){
+			const frames = ["Sprites_assets/Player/Walking/1.png", "Sprites_assets/Player/Walking/2.png", "Sprites_assets/Player/Walking/3.png", "Sprites_assets/Player/Walking/4.png","Sprites_assets/Player/Walking/5.png","Sprites_assets/Player/Walking/6.png","Sprites_assets/Player/Walking/7.png"]; 
+			const frameRate = 100; // Vitesse de l'animation ==> 50ms par frame
+			let index = 0;	
+			const Interval_Walking_animation = setInterval(() => {
+				index = (index + 1) % frames.length;
+				drawingPlayer.style.backgroundImage = `url(${frames[index]})`;
+				if ((moving === false) || (facing==="right")) {
+					clearInterval(Interval_Walking_animation);
+					animation_Joueur();
+				}
+		}, frameRate);
+		} else {
+			const frames = ["Sprites_assets/Player/Idle/1.png", "Sprites_assets/Player/Idle/2.png", "Sprites_assets/Player/Idle/3.png", "Sprites_assets/Player/Idle/4.png","Sprites_assets/Player/Idle/5.png","Sprites_assets/Player/Idle/6.png","Sprites_assets/Player/Idle/7.png","Sprites_assets/Player/Idle/8.png","Sprites_assets/Player/Idle/9.png","Sprites_assets/Player/Idle/10.png"]; 
+			const frameRate = 100; // Vitesse de l'animation ==> 50ms par frame
+			let index = 0;	
+			const Interval_Idle_animation = setInterval(() => {
+				index = (index + 1) % frames.length;
+				drawingPlayer.style.backgroundImage = `url(${frames[index]})`;
+				if (moving === true) {
+					clearInterval(Interval_Idle_animation);
+					animation_Joueur();
+				}
+		}, frameRate);
+		}
+	}
+	if (facing==="right"){
+		drawingPlayer.style.transform = `scale(-1, 1)`;
+	
+	if (moving ===true){
+		const frames = ["Sprites_assets/Player/Walking/1.png", "Sprites_assets/Player/Walking/2.png", "Sprites_assets/Player/Walking/3.png", "Sprites_assets/Player/Walking/4.png","Sprites_assets/Player/Walking/5.png","Sprites_assets/Player/Walking/6.png","Sprites_assets/Player/Walking/7.png"]; 
+		const frameRate = 100; // Vitesse de l'animation ==> 50ms par frame
+		let index = 0;	
+		const Interval_Walking_animation = setInterval(() => {
+			index = (index + 1) % frames.length;
+			drawingPlayer.style.backgroundImage = `url(${frames[index]})`;
+			if ((moving === false) || (facing==="left")) {
+				clearInterval(Interval_Walking_animation);
+				animation_Joueur();
+			}
+	}, frameRate);
+	} else {
+		const frames = ["Sprites_assets/Player/Idle/1.png", "Sprites_assets/Player/Idle/2.png", "Sprites_assets/Player/Idle/3.png", "Sprites_assets/Player/Idle/4.png","Sprites_assets/Player/Idle/5.png","Sprites_assets/Player/Idle/6.png","Sprites_assets/Player/Idle/7.png","Sprites_assets/Player/Idle/8.png","Sprites_assets/Player/Idle/9.png","Sprites_assets/Player/Idle/10.png"]; 
+		const frameRate = 100; // Vitesse de l'animation ==> 50ms par frame
+		let index = 0;	
+		const Interval_Idle_animation = setInterval(() => {
+			index = (index + 1) % frames.length;
+			drawingPlayer.style.backgroundImage = `url(${frames[index]})`;
+			if (moving === true) {
+				clearInterval(Interval_Idle_animation);
+				animation_Joueur();
+			}
+	}, frameRate);
+	}
+}
+}
+animation_Joueur();
+
+// On charge les sprites pour charger les animations.(On peut mettre cette partie du code à la fin, un peu longue et très peu intéressante.)
+
+//setInterval(() => {
+// On charge le Nuage
+const Cloud_frames = ["Sprites_assets/Boss/Cloud_Idle/1.png", "Sprites_assets/Boss/Cloud_Idle/2.png", "Sprites_assets/Boss/Cloud_Idle/3.png", "Sprites_assets/Boss/Cloud_Idle/4.png"]; 
+const Cloud_frameRate = 50; // Vitesse de l'animation ==> 50ms par frame
+let Cloud_index = 0;	
+setInterval(() => {
+	Cloud_index = (Cloud_index + 1) % Cloud_frames.length;
+	LoadingCloud.style.backgroundImage = `url(${Cloud_frames[Cloud_index]})`;
+}, Cloud_frameRate);
+
+
+// On charge l'attaque du Nuage
+const Cloud_Attack_frames = ["Sprites_assets/Boss/Attack/1.png", "Sprites_assets/Boss/Attack/2.png", "Sprites_assets/Boss/Attack/3.png", "Sprites_assets/Boss/Attack/4.png", "Sprites_assets/Boss/Attack/5.png", "Sprites_assets/Boss/Attack/6.png", "Sprites_assets/Boss/Attack/7.png", "Sprites_assets/Boss/Attack/8.png","Sprites_assets/Boss/Attack/9.png"]; 
+const Cloud_Attack_frameRate = 50; // Vitesse de l'animation ==> 50ms par frame
+let Cloud_Attack_index = 0;	
+setInterval(() => {
+    Cloud_Attack_index = (Cloud_Attack_index + 1) % Cloud_Attack_frames.length;
+    LoadingAttackCloud.style.backgroundImage = `url(${Cloud_Attack_frames[Cloud_Attack_index]})`;
+}, Cloud_Attack_frameRate);
+
+
+// On charge l'animation de marche du player
+const Player_Walking_frames = ["Sprites_assets/Player/Walking/1.png", "Sprites_assets/Player/Walking/2.png", "Sprites_assets/Player/Walking/3.png", "Sprites_assets/Player/Walking/4.png","Sprites_assets/Player/Walking/5.png","Sprites_assets/Player/Walking/6.png","Sprites_assets/Player/Walking/7.png"];
+const Player_Walking_frameRate = 50; // Vitesse de l'animation ==> 50ms par frame
+let Player_Walking_index = 0;	
+setInterval(() => {
+    Player_Walking_index = (Player_Walking_index + 1) % Player_Walking_frames.length;
+    LoadingWalkingPlayer.style.backgroundImage = `url(${Player_Walking_frames[Player_Walking_index]})`;
+}, Player_Walking_frameRate);
+
+// On charge l'animation d'attente du player
+const Player_Idle_frames = ["Sprites_assets/Player/Idle/1.png", "Sprites_assets/Player/Idle/2.png", "Sprites_assets/Player/Idle/3.png", "Sprites_assets/Player/Idle/4.png","Sprites_assets/Player/Idle/5.png","Sprites_assets/Player/Idle/6.png","Sprites_assets/Player/Idle/7.png","Sprites_assets/Player/Idle/8.png","Sprites_assets/Player/Idle/9.png","Sprites_assets/Player/Idle/10.png"];
+const Player_Idle_frameRate = 50; // Vitesse de l'animation ==> 50ms par frame
+let Player_Idle_index = 0;	
+setInterval(() => {
+    Player_Idle_index = (Player_Idle_index + 1) % Player_Idle_frames.length;
+    LoadingIdlePlayer.style.backgroundImage = `url(${Player_Idle_frames[Player_Idle_index]})`;
+}, Player_Idle_frameRate);
+
+// On charge le Zombie
+const Zombie_frames = ["Sprites_assets/Boss/Zombie/1.png", "Sprites_assets/Boss/Zombie/2.png", "Sprites_assets/Boss/Zombie/3.png", "Sprites_assets/Boss/Zombie/4.png","Sprites_assets/Boss/Zombie/5.png","Sprites_assets/Boss/Zombie/6.png","Sprites_assets/Boss/Zombie/7.png"]; 
+const Zombie_frameRate = 50; // Vitesse de l'animation ==> 50ms par frame
+let Zombie_index = 0;	
+setInterval(() => {
+	Zombie_index = (Zombie_index + 1) % Zombie_frames.length;
+	LoadingZombie.style.backgroundImage = `url(${Zombie_frames[Zombie_index]})`;
+}, Zombie_frameRate);
+
+// On charge le Nuage "pluie"
+const SlowCloud_frames = ["Sprites_assets/Boss/Cloud_Idle_Rain/1.png", "Sprites_assets/Boss/Cloud_Idle_Rain/2.png", "Sprites_assets/Boss/Cloud_Idle_Rain/3.png", "Sprites_assets/Boss/Cloud_Idle_Rain/4.png"]; 
+const SlowCloud_frameRate = 50; // Vitesse de l'animation ==> 50ms par frame
+let SlowCloud_index = 0;	
+setInterval(() => {
+	SlowCloud_index = (SlowCloud_index + 1) % SlowCloud_frames.length;
+	LoadingSlowCloud.style.backgroundImage = `url(${SlowCloud_frames[SlowCloud_index]})`;
+}, SlowCloud_frameRate);
 
 
 //end game
@@ -266,7 +405,7 @@ document.addEventListener("keydown", (event) => {
         case 100:
             console.log("ATTAQUE 4");
 		    if (can_attack4) {
-				alert('Je rajoute la dernière attaque plus tard.');
+				attack4();
 			}
             break;
     }
@@ -376,31 +515,53 @@ function spawn_moving_cloud(){
 	CanCloud_move = true; // Le nuage peut bouger
     moving_cloud.style.display = 'block'; //Le nuage est visible
     moving_cloud.classList.add('warninghitbox');
+	drawingMovingCloud.style.display = 'block'; // Le dessin / sprite devient visible
+	const Cloud_frames = ["Sprites_assets/Boss/Cloud_Idle/1.png", "Sprites_assets/Boss/Cloud_Idle/2.png", "Sprites_assets/Boss/Cloud_Idle/3.png", "Sprites_assets/Boss/Cloud_Idle/4.png"]; 
+	const Cloud_frameRate = 50; // Vitesse de l'animation ==> 50ms par frame
+	let Cloud_index = 0;	
+	Interval_first_animation =setInterval(() => {
+		Cloud_index = (Cloud_index + 1) % Cloud_frames.length;
+		drawingMovingCloud.style.backgroundImage = `url(${Cloud_frames[Cloud_index]})`;
+	}, Cloud_frameRate);
 
     // HURTHITBOX
     setTimeout(() => {
 		CanCloud_move = false; // Stop movement
+		clearInterval(Interval_first_animation);
+		const Cloud_Attack_frames = ["Sprites_assets/Boss/Attack/1.png", "Sprites_assets/Boss/Attack/2.png", "Sprites_assets/Boss/Attack/3.png", "Sprites_assets/Boss/Attack/4.png", "Sprites_assets/Boss/Attack/5.png", "Sprites_assets/Boss/Attack/6.png", "Sprites_assets/Boss/Attack/7.png", "Sprites_assets/Boss/Attack/8.png","Sprites_assets/Boss/Attack/9.png"]; 
+		const Cloud_Attack_frameRate = 50; // Vitesse de l'animation ==> 50ms par frame
+		let Cloud_Attack_index = 0;	
+		Interval_second_animation = setInterval(() => {
+			Cloud_Attack_index = (Cloud_Attack_index + 1) % Cloud_Attack_frames.length;
+			drawingMovingCloud.style.backgroundImage = `url(${Cloud_Attack_frames[Cloud_Attack_index]})`;
+		}, Cloud_Attack_frameRate);
+
+	}, 2100);
+
+    setTimeout(() => {
 		moving_cloud.classList.remove('warninghitbox');
         moving_cloud.classList.add('hurthitbox');
 		degat = 'True'
 		coord = [moving_cloudState.x, moving_cloudState.y];
 		collision.push(coord);
-    }, 2000);
+		
+    }, 2350);
 
     setTimeout(() => {
         moving_cloud.style.display = 'none'; // On cache le nuage
+		drawingMovingCloud.style.display='none';
+		drawingMovingCloud.style.backgroundImage="Sprites_assets/Boss/Cloud_Idle/1.png";
+		clearInterval(Interval_second_animation);
+
 		moving_cloud.classList.remove('hurthitbox');
-        // On change sa position pour qu'elle soit random
-		//const XC= RandomNumber(0,598);
-		//const YC= RandomNumber(0,235);
-        moving_cloudState.x = 260;
-        moving_cloudState.y = 100;
-        moving_cloud.style.left = `${moving_cloudState.x}px`;
-        moving_cloud.style.top = `${moving_cloudState.y}px`;
+	        moving_cloudState.x = 260;
+	        moving_cloudState.y = 100;
+	        moving_cloud.style.left = `${moving_cloudState.x}px`;
+	        moving_cloud.style.top = `${moving_cloudState.y}px`;
 		collision = [];
 		collision.length = 0;
 		degat = 'False';
-    }, 2500);
+    }, 2600);
 }
 
 function attack2(){
@@ -428,20 +589,56 @@ function reload3(){
 
 function spawn_box3(){
 	console.log("spawn a box 3");
-	const c= RandomNumber(9,31);
-	const r= RandomNumber(19,28);
+	let c= RandomNumber(0,195); // (9,31) anciennes valeurs, en fonction de la gameBox
+	let r= RandomNumber(0,420); // (19,28)
 	const slowhitbox = document.createElement('div'); // On crée l'élément div slowhitbox
+	const draw_slow_cloud = document.createElement('div'); // On dessine le sprite associé
 	slowhitbox.classList.add("warningslowhitbox");
-	slowhitbox.style.gridRow = `${r}`;
-    slowhitbox.style.gridColumn = `${c}`;
+	draw_slow_cloud.classList.add("drawingSlowCloud");
+	slowhitbox.style.left = `${r}px`; //gridRow
+    	slowhitbox.style.top = `${c}px`; //gridColumn
+	draw_slow_cloud.style.left = `${r-118}px`;
+   	draw_slow_cloud.style.top = `${c-252}px`;
 	console.log(r);
 	console.log(c);
-	gameBox.appendChild(slowhitbox); // On ajoute l'élément sur la boîte de jeu
+	gameWorld.appendChild(slowhitbox); // On ajoute l'élément sur la boîte de jeu //gameWorld
+	gameWorld.appendChild(draw_slow_cloud);
+	// On dessine l'attaque du Nuage frame par frame
+	const Cloud_frames = ["Sprites_assets/Boss/Cloud_Idle_Rain/1.png", "Sprites_assets/Boss/Cloud_Idle_Rain/2.png", "Sprites_assets/Boss/Cloud_Idle_Rain/3.png", "Sprites_assets/Boss/Cloud_Idle_Rain/4.png"]; 
+	const Cloud_frameRate = 50; // Vitesse de l'animation ==> 50ms par frame
+	let Cloud_index = 0;	
+	const Interval_first_animation = setInterval(() => {
+		Cloud_index = (Cloud_index + 1) % Cloud_frames.length;
+		draw_slow_cloud.style.backgroundImage = `url(${Cloud_frames[Cloud_index]})`;
+	}, Cloud_frameRate);
+	// On ajoute les goutes de pluies avec deux set_Interval
+	const Rain_animation = setInterval(() => {
+		const Rain = document.createElement('div'); // On dessine le sprite associé
+		Rain.classList.add("Rain");
+		let Rr= RandomNumber(-40,40); 
+		let Rc= RandomNumber(-12,12); 
+		Rain.style.left = `${r-118+Rr}px`;
+    	Rain.style.top = `${c-252+Rc}px`;
+		gameWorld.appendChild(Rain);
+		const Rain_frames = ["Sprites_assets/Boss/Rain_Drop_Animation/1.png", "Sprites_assets/Boss/Rain_Drop_Animation/2.png", "Sprites_assets/Boss/Rain_Drop_Animation/3.png", "Sprites_assets/Boss/Rain_Drop_Animation/4.png", "Sprites_assets/Boss/Rain_Drop_Animation/5.png", "Sprites_assets/Boss/Rain_Drop_Animation/6.png", "Sprites_assets/Boss/Rain_Drop_Animation/7.png", "Sprites_assets/Boss/Rain_Drop_Animation/8.png", "Sprites_assets/Boss/Rain_Drop_Animation/9.png", "Sprites_assets/Boss/Rain_Drop_Animation/10.png", "Sprites_assets/Boss/Rain_Drop_Animation/11.png", "Sprites_assets/Boss/Rain_Drop_Animation/12.png", "Sprites_assets/Boss/Rain_Drop_Animation/13.png", "Sprites_assets/Boss/Rain_Drop_Animation/14.png", "Sprites_assets/Boss/Rain_Drop_Animation/15.png", "Sprites_assets/Boss/Rain_Drop_Animation/16.png", "Sprites_assets/Boss/Rain_Drop_Animation/17.png"]; 
+		const Rain_frameRate = 30; // Vitesse de l'animation ==> 30ms par frame
+		let Rain_index = 0;	
+		const Falling_Rain_animation = setInterval(() => {
+			Rain_index = Rain_index + 1;
+			Rain.style.backgroundImage = `url(${Rain_frames[Rain_index]})`;
+			if (Rain_index === Rain_frames.length){
+				Rain.remove();
+				clearInterval(Falling_Rain_animation);
+			}
+		}, Rain_frameRate);
+	}, 20);
 	setTimeout(() => {
 		slowhitbox.className = "slowhitbox";
+		clearInterval(Rain_animation);
 	}, 1200);
 	setTimeout(() => {
 		slowhitbox.remove();
+		draw_slow_cloud.remove();
 	}, 1500);
 }
 
@@ -450,7 +647,7 @@ function attack3(){
 	console.log("oui je marche");
 	attack3_loading.className = "attack_loading";
 	can_attack3 = false;
-	for (i=0;i<5; i++){
+	for (i=0;i<10; i++){
 		spawn_box3();
 	}
 	reload3();
@@ -469,4 +666,95 @@ function reload4(){
 		filler4.style.width = "0";
 		can_attack4 = true;
 	}, 10000);
+}
+
+function attack4(){
+	let attack4_loading = document.getElementById("AL4");
+	attack4_loading.className = "attack_loading";
+	can_attack4 = false;
+	for (i=0;i<5; i++){
+		spawn_Zombies();
+	}
+	reload4();
+}
+
+const zombies = [];
+function spawn_Zombies(){
+	console.log("spawn a zombie");
+	const y= RandomNumber(0,213);
+	const x= RandomNumber(0,478);
+	const Zombie = document.createElement('div'); // On crée l'élément div Zombie
+	Zombie.classList.add("zombie"); 
+	Zombie.style.top = `${y}px`;
+   	Zombie.style.left = `${x}px`;
+
+	// On dessine le Zombie
+	const Draw_Zombie = document.createElement('div'); // On dessine le sprite associé
+	Draw_Zombie.classList.add("drawingZombie");
+	Draw_Zombie.style.left = Zombie.style.left;
+	Draw_Zombie.style.top = Zombie.style.top;
+	gameWorld.appendChild(Draw_Zombie);
+	const Zombie_Walking_frames = ["Sprites_assets/Boss/Zombie/1.png", "Sprites_assets/Boss/Zombie/2.png", "Sprites_assets/Boss/Zombie/3.png", "Sprites_assets/Boss/Zombie/4.png","Sprites_assets/Boss/Zombie/5.png","Sprites_assets/Boss/Zombie/6.png","Sprites_assets/Boss/Zombie/7.png"];
+	const Zombie_Walking_frameRate = 200; // Vitesse de l'animation ==> 50ms par frame
+	let Zombie_Walking_index = 0;	
+	setInterval(() => {
+		Zombie_Walking_index = (Zombie_Walking_index + 1) % Zombie_Walking_frames.length;
+		Draw_Zombie.style.backgroundImage = `url(${Zombie_Walking_frames[Zombie_Walking_index]})`;
+	}, Zombie_Walking_frameRate);
+	
+	gameWorld.appendChild(Zombie); // On ajoute l'élément sur la boîte de jeu
+	zombies.push({
+		element: Zombie,
+		x: x,
+		y: y,
+		drawing: Draw_Zombie,
+	}); // Ajoute le tuple (element, x, y) dans la liste zombies. (pb => il faut l'index du zombie pour le tuer)
+}
+
+function moveZombiesTowardsPlayer() {
+	// On récupère la position du joueur sur la grille. Pour cela, on utilise ses coordonnées (x,y). On divise par 20 car c'est la taille de la grille (20px) et on ajoute un décalage que j'ai trouvé manuellement.
+	const playerRow = Math.round(playerState.y / 20) +17; 
+	const playerCol = Math.round(playerState.x / 20) +5;
+
+	// Pour chaque élément zombie dans la liste zombies.
+	zombies.forEach((zombie, index) => {
+		const Zombie = zombie.element;
+		const Draw_Zombie = zombie.drawing;
+
+		// on regarde la direction du joueur à l'aide de différences.
+		const coordYDiff = playerState.y - zombie.y; // Si cette valeur est négative alors le joueur est en haut, si elle est positive, le joueur est en bas
+		const coordXDiff = playerState.x - zombie.x; // Si cette valeur est négative alors le joueur est à gauche, si elle est positive, le joueur est à droite
+
+		// On normalise le pas du zombie.
+		const moveRow = coordYDiff === 0 ? 0 : coordYDiff / Math.abs(coordYDiff);
+		const moveCol = coordXDiff === 0 ? 0 : coordXDiff / Math.abs(coordXDiff);
+
+
+		// On met à jour la position du Zombie
+		zombie.y += moveRow;
+		zombie.x += moveCol;
+
+		// On met à jour la position du Zombie sur la grille
+		Zombie.style.top = `${zombie.y}px`;
+		Zombie.style.left = `${zombie.x}px`;
+
+		if (coordXDiff<0){
+			Draw_Zombie.style.transform = `scale(-1, 1)`;
+			Draw_Zombie.style.top = `${zombie.y-60}px`;
+			Draw_Zombie.style.left = `${zombie.x-26}px`;
+		} else {
+			Draw_Zombie.style.transform = `scale(1, 1)`;
+			Draw_Zombie.style.top = `${zombie.y-60}px`;
+			Draw_Zombie.style.left = `${zombie.x-36}px`;
+		}
+
+	});
+}
+
+// On appelle la fonction toute les 50 ms
+setInterval(moveZombiesTowardsPlayer, 50);
+
+function killZombie(zombieIndex) {
+    const zombie = zombies[zombieIndex];
+
 }
