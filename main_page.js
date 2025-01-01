7e6ac5eda817c9eeeec905ae5f8a15228c1f323e
Anthony97480 <tony.ferrere.h@gmail.com>
2025-01-01 14:57:31 +0100

    Update main_page.js

--- main_page.js
+++ main_page.js
@@ -36,7 +36,7 @@ function Pseudo_ok(pseudo){
 function StartGame(){ //fonction de redirection vers la page du jeux
 	const Jpseudo1 = document.getElementById("idpseudo1").value;
 	const Jpseudo2 = document.getElementById("idpseudo2").value;
-	if (Pseudo_ok(Jpseudo1) && Pseudo_ok(Jpseudo2)){
+	if (Pseudo_ok(Jpseudo1 && Jpseudo2)){
 		localStorage.setItem('pseudo_perso1', Jpseudo1); //stockage du pseudo
 		localStorage.setItem('pseudo_perso2', Jpseudo2); //stockage du pseudo
 		window.location.href = "main_game.html" //redirection sur la page du jeux
